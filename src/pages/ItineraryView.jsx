4d461b1740be994a1ab357c55bd221a7ff28fa5c
priyank197 <priyankdomadiya347@gmail.com>
2026-08-22 17:33:35 +0530

    Fix leaflet import in ItineraryView to resolve blank screen

--- src/pages/ItineraryView.jsx
+++ src/pages/ItineraryView.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { supabase, fetchAllTrips, fetchAllStops, SEED_CITIES, SEED_ACTIVITIES } from '../lib/supabase'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 // Leaflet default icon fix
-delete L.Icon.Default.prototype._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png'
-})
+if (typeof window !== 'undefined' && L && L.Icon && L.Icon.Default) {
+  delete L.Icon.Default.prototype._getIconUrl
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png'
+  })
+}
 
 export default function ItineraryView() {
   const { id } = useParams()
